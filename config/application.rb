602d7ccd86e917d19df8596ecb3044f667094227
ytyeoh <ytyeoh@me.com>
2016-08-19 09:08:58 +0800

    set time zone

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.time_zone = '"Asia/Kuala_Lumpur'
-config.active_record.default_timezone = :local
 module Puser
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Asia/Kuala_Lumpur'
+    config.active_record.default_timezone = :local
   end
 end
